49ea45a0bb63240d0b7ff7d4b398c3fb7b61f482
Dewayne VanHoozer <Dewayne@frank.(none)>
2012-07-30 16:20:47 -0500

    proof of concept

--- app.rb
+++ app.rb
@@ -3,11 +3,80 @@ APP_ROOT = File.expand_path(File.dirname(__FILE__))
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'erb'
 
 set :root, APP_ROOT
+set :public_folder, File.dirname(__FILE__) + '/public'
 
+#####################################
 get '/' do
-  haml :index
+  erb :index
+end
+
+get '/index' do
+  erb :index
+end
+
+get '/home' do
+  erb :index
+end
+
+#####################################
+get '/bio' do
+  erb :bio
+end
+
+#####################################
+get '/roles' do
+  erb :roles
+end
+
+get '/roles_:name' do
+  erb :"roles_#{params[:name]}"
+end
+
+#####################################
+get '/industries' do
+  erb :industries
+end
+
+get '/industries_:name' do
+  erb :"industries_#{params[:name]}"
+end
+
+#####################################
+get '/projects' do
+  erb :projects
+end
+
+get '/projects_:name' do
+  erb :"projects_#{params[:name]}"
+end
+
+#####################################
+get '/technologies' do
+  erb :technologies
+end
+
+#####################################
+get '/contact' do
+  erb :contact
+end
+
+
+
+
+###########################################
+get '/page.html' do
+  erb :page
+end
+
+get '/another_page.html' do
+  erb :another_page
+end
+
+get '/examples.html' do
+  erb :examples
 end
 
 
