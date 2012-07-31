0e26d772434c8135589cd83abcf0fe9773e0dc14
Dewayne VanHoozer <Dewayne@frank.(none)>
2012-07-31 01:04:18 -0500

    forgot to add the modified files

--- app.rb
+++ app.rb
@@ -8,75 +8,65 @@ require 'erb'
 set :root, APP_ROOT
 set :public_folder, File.dirname(__FILE__) + '/public'
 
+
+#####################################
+not_found { erb :'404' }
+error { @error = request.env['sinatra_error'] ; erb :'500' }
+
+
 #####################################
 get '/' do
   erb :index
 end
 
-get '/index' do
+get '/index/?' do
   erb :index
 end
 
-get '/home' do
+get '/home/?' do
   erb :index
 end
 
 #####################################
-get '/bio' do
+get '/bio/?' do
   erb :bio
 end
 
 #####################################
-get '/roles' do
+get '/roles/?' do
   erb :roles
 end
 
-get '/roles_:name' do
+get '/roles_:name/?' do
   erb :"roles_#{params[:name]}"
 end
 
 #####################################
-get '/industries' do
+get '/industries/?' do
   erb :industries
 end
 
-get '/industries_:name' do
+get '/industries_:name/?' do
   erb :"industries_#{params[:name]}"
 end
 
 #####################################
-get '/projects' do
+get '/projects/?' do
   erb :projects
 end
 
-get '/projects_:name' do
+get '/projects_:name/?' do
   erb :"projects_#{params[:name]}"
 end
 
 #####################################
-get '/technologies' do
+get '/technologies/?' do
   erb :technologies
 end
 
 #####################################
-get '/contact' do
+get '/contact/?' do
   erb :contact
-end
-
-
-
-
-###########################################
-get '/page.html' do
-  erb :page
-end
-
-get '/another_page.html' do
-  erb :another_page
-end
-
-get '/examples.html' do
-  erb :examples
 end
 
 
